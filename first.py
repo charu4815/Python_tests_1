259fe949802c85628396444eefe978f6d15655bb
charu4815 <nikhadecharudatta1997@gmail.com>
2026-01-12 00:03:56 +0530

    First Push for github repo

---
+++ first.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import random
+
+
+arr1 = [1,2,3,4,5]
+
+arr1 = np.array(arr1)
+
+arr = np.random.randint(1,50,10)
+
+lst = [1,2,3]
+
+print(type(arr))
+# print(arr1*5,lst*5)
